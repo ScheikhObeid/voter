1a34510d177966adc82cffc0c8fe4343d5763636
scheikh <abdulrahman404@hotmail.com>
2022-04-08 02:12:33 +0200

    v-for, show list of all array elemnts

--- app.js
+++ app.js
@@ -2,7 +2,41 @@ const app = Vue.createApp({
   data: function () {
     return {
       submissions: submissions,
+      totalVotes: 0,
     };
+  },
+  // computed: {
+  //   totalVotes() {
+  //     console.log("computed property ausgeführt.");
+  //     return this.submissions.reduce((totalVotes, submission) => {
+  //       return totalVotes + submission.votes;
+  //     }, 0);
+  //   },
+  // },
+
+  watch: {
+    // submissions(newValue, oldValue) {
+    //   console.log(newValue);
+    //   console.log(oldValue);
+    // },
+    submissions: {
+      handler(newValue, oldValue) {
+        this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
+          return totalVotes + submission.votes;
+        }, 0);
+      },
+      deep: true,
+      immediate: true,
+    },
+    totalVotes(newValue, oldValue) {
+      console.log(newValue);
+      console.log(oldValue);
+    },
+  },
+  methods: {
+    upvote() {
+      this.submissions[0].votes++;
+    },
   },
 });
 
